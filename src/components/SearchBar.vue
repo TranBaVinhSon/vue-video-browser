d12eb67691180a861c1e4de9c34205daa6be38f4
Tran B.V. Son <tranbavinhson@gmail.com>
2018-05-12 19:49:08 +0700

    first commit

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div>
+    <input @input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  data: {},
+  methods: {
+    onInput: function(event) {
+      console.log(event.target.value);
+      this.$emit("termChange", event.target.value);
+    }
+  }
+};
+</script>
+
+<style lang="css">
+</style>
